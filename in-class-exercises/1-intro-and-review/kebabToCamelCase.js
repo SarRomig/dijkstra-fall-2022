f090af14befa71deecc03734af7abb7cf14bbc9b
SarRomig <107075253+SarRomig@users.noreply.github.com>
2022-10-03 10:08:03 -0400

    finished function kebabToCamel

--- in-class-exercises/1-intro-and-review/kebabToCamelCase.js
+++ in-class-exercises/1-intro-and-review/kebabToCamelCase.js
@@ -10,10 +10,19 @@
 
 
 function kebabToCamel(kebabString) {
-    let camelString;
+    let camelString = "";
 
 //Loop over kebabString. If char is a dash, don't use it in camelString and the following will be capitalized. 
-//Otherwise, copy lowercase to camelString
+    for (let i = 0; i < kebabString.length; i++) {
+        if (kebabString[i] == "-") {
+        i++; //to move it over ("skip" the dash)
+        camelString += kebabString[i].toUpperCase();
+        }
+        else {
+            //Otherwise, copy lowercase to camelString
+            camelString += kebabString[i];
+        }
+    }
     return camelString;
 }
-
+console.log(kebabToCamel("hello-boot-camp-2022"));
